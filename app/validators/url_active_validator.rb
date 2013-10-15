b0f2c21f59a32cdd192fe1ea7e027f785ebe02cd
ao3 <elzj78+ao3@gmail.com>
2013-10-15 17:48:25 -0400

    Refactoring url validator

--- app/validators/url_active_validator.rb
+++ app/validators/url_active_validator.rb
@@ -9,19 +9,21 @@ class UrlActiveValidator < ActiveModel::EachValidator
   def validate_each(record,attribute,value)
     return true if value.match("fanfiction.net")
     inactive_url_msg = "could not be reached. If the URL is correct and the site is currently down, please try again later."
-    inactive_url_timeout = 10 # seconds
+    record.errors[attribute] << inactive_url_msg unless active?(value)
+  end
 
+  def active?(value)
+    url = URI.parse(value)
+    url_timeout = 10 # seconds
     begin
-      status = Timeout::timeout(options[:timeout] || inactive_url_timeout) {
-        url = URI.parse(value)
+      Timeout::timeout(url_timeout) {
         response_code = Net::HTTP.start(url.host, url.port) {|http| http.head(url.path.blank? ? '/' : url.path).code}
         active_status = %w(200 301 302)
         active_status.include? response_code
       }
-    rescue 
-      status = false
+    rescue Timeout::Error
+      false
     end
-    record.errors[attribute] << (options[:message] || inactive_url_msg) unless status
   end
     
 end
